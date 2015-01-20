dc0e93047995cdf8ad7e09165d6ceb68ebed610b
Tristan Colgate-McFarlane <tcolgate@gmail.com>
2015-01-20 21:15:29 +0000

    Add example from docs as oauth test

--- pkg/mediagraft/oauth/oauth_test.go
+++ pkg/mediagraft/oauth/oauth_test.go
@@ -1,1 +1,23 @@
 package oauth
+
+// From the docs
+//
+// IZAxYqW3gyxYMoXy7cAu33VH52slX6TfbxHEjajECUi6EOGH4dhN9Cy++tJ3iI\/WsqrSq04CM+S4Yu4R2QZBZQ==
+// 1312471030
+// gfn2lfvn5asfo
+//
+// GET
+// api.we7.com
+// 80
+// /api/0.1/userPlaylistsInfo
+// apiKey=myKey
+// appVersion=1
+// detail=full
+// format=xml
+//
+//
+// Gives
+//
+// Authorization: MAC token="IZAxYqW3gyxYMoXy7cAu33VH52slX6TfbxHEjajECUi6EOGH4dhN9Cy++tJ3iI\/WsqrSq04CM+S4Yu4R2QZBZQ==",timestamp="1312472895&",nonce="gfn2lfvn5asfo",signature="38kvZAJcf+Xq+W/Zs+7nG9ClZnI="
+//
+// http://api.we7.com/api/0.1/userPlaylistsInfo?appVersion=1&apiKey=myKey&format=xml&detail=full
