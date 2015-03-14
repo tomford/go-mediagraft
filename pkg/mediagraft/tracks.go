26aa012261c6093a93de88188613c60c4688cc16
Tristan Colgate-McFarlane <tcolgate@gmail.com>
2015-03-14 19:42:02 +0000

    Add bindings for TracksInfo call

---
+++ pkg/mediagraft/tracks.go
@@ -0,0 +1,59 @@
+package mediagraft
+
+import (
+	"encoding/json"
+	"net/url"
+	"strconv"
+	"strings"
+)
+
+func (c *Client) TracksInfo(trackId ...int32) ([]Track, error) {
+	args := &url.Values{}
+
+	var strids []string
+	for _, t := range trackId {
+		strids = append(strids, strconv.Itoa(int(t)))
+	}
+	args.Set("ids", strings.Join(strids, ","))
+
+	r, err := c.Call("GET", "tracksInfo", args, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var t []Track
+	//io.Copy(os.Stdout, r.Body)
+	dec := json.NewDecoder(r.Body)
+	err = dec.Decode(&t)
+	if err != nil {
+		return nil, err
+	}
+
+	return t, nil
+}
+
+func (c *Client) TrackVersionsInfo(trackId ...int32) ([]Track, error) {
+	args := &url.Values{}
+
+	var strids []string
+	for _, t := range trackId {
+		strids = append(strids, strconv.Itoa(int(t)))
+	}
+
+	args.Set("versionids", strings.Join(strids, ","))
+
+	r, err := c.Call("GET", "tracksInfo", args, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var t []Track
+	//io.Copy(os.Stdout, r.Body)
+	dec := json.NewDecoder(r.Body)
+	err = dec.Decode(&t)
+	if err != nil {
+		return nil, err
+	}
+
+	return t, nil
+}
