f4aad3aaa2a80263e774fafc9e111ed614859540
Tristan Colgate-McFarlane <tcolgate@gmail.com>
2015-03-15 17:57:12 +0000

    Add artistsinfo and albumsinfo calls

---
+++ pkg/mediagraft/albums.go
@@ -0,0 +1,35 @@
+package mediagraft
+
+import (
+	"encoding/json"
+	"net/url"
+	"strconv"
+	"strings"
+)
+
+func (c *Client) AlbumsInfo(albumId ...int32) ([]Album, error) {
+	args := &url.Values{}
+
+	var strids []string
+	for _, t := range albumId {
+		strids = append(strids, strconv.Itoa(int(t)))
+	}
+	args.Set("ids", strings.Join(strids, ","))
+	args.Set("detail", "full")
+
+	r, err := c.Call("GET", "artistsInfo", args, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer r.Body.Close()
+
+	var a []Album
+	//io.Copy(os.Stdout, r.Body)
+	dec := json.NewDecoder(r.Body)
+	err = dec.Decode(&a)
+	if err != nil {
+		return nil, err
+	}
+
+	return a, nil
+}
